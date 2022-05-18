aa5d8dd178ffa86aa8c7376b202c37e867b97cff
Chen <359000081@qq.com>
2022-05-19 07:23:02 +0800

    print out appName and version;

--- helper.go
+++ helper.go
@@ -1,6 +1,10 @@
 package main
 import "fmt"
 
+func appName()  {
+	fmt.Println("calogoflavorings")
+}
+
 func version()  {
 	fmt.Println("1.0.0")
 }
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ import "fmt"
 
 func main() {
   fmt.Println("go flavorings...")
+  appName()
   version()
 }
