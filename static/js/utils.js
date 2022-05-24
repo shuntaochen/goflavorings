6d6c47dfcb84b0c75d1055671c0fa2b3b7810e6c
Chen <359000081@qq.com>
2022-05-24 19:33:43 +0800

    add gethtmlorjson method;

--- static/js/utils.js
+++ static/js/utils.js
@@ -15,3 +15,19 @@ function loadScripts() {
 function redirect(path) {
     location.href = path
 }
+
+function getHtmlOrJson(success) {
+    function makeHttpObject() {
+        if ("XMLHttpRequest" in window) return new XMLHttpRequest();
+        else if ("ActiveXObject" in window) return new ActiveXObject("Msxml2.XMLHTTP");
+    }
+
+    var request = makeHttpObject();
+    request.open("GET", "/", true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState == 4)
+            console.log(request.responseText);
+        success(request.responseText)
+    };
+}
