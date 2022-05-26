d8d1335cdbfa6a4f98e49e5a6d77eb9ba70665f1
Chen <359000081@qq.com>
2022-05-26 09:37:07 +0800

    update;

--- spa/spaExtension.js
+++ spa/spaExtension.js
@@ -1,9 +1,11 @@
 (function (o) {
 
+    console.log('elroot', o)
     const root = o.rootel
     o.templateStore = {}
-    o.router = o.router || { '/routea': 'templateA.calo' }
-
+    const templateStore = o.templateStore
+    o.router = o.router || { '/routea': 'templatea.html' }
+    const router = o.router
     for (const key in router) {
         if (Object.hasOwnProperty.call(router, key)) {
             const htmlName = router[key];
@@ -12,10 +14,19 @@
             })
         }
     }
+
+    var stringToHTML = function (str) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(str, 'text/html');
+        return doc.body;
+    };
     o.navigate = function (route) {
         window.history.pushState(null, null, route)
-        root.innerHTML = decodeURI(templateStore[route])
-        o.run.apply(o)
+        root.innerHTML = ''
+        var hm = stringToHTML(decodeURI(templateStore[route]))
+        var script = hm.getElementsByTagName('script')[0].text;
+        root.appendChild(hm)
+        eval(script)
     }
     window.calo = o || {
         model: {}
