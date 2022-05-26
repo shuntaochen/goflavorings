745ff1f770694869968fd7e3dc26d2205ee43e76
Chen <359000081@qq.com>
2022-05-26 08:49:19 +0800

    Update spaExtension.js;

--- spa/spaExtension.js
+++ spa/spaExtension.js
@@ -8,13 +8,14 @@
     for (const key in router) {
         if (Object.hasOwnProperty.call(router, key)) {
             const htmlName = router[key];
-            templateStore[key] = '';
-
+            getHtmlOrJson('./views/' + htmlName, function (text) {
+                templateStore[key] = encodeURI(text);
+            })
         }
     }
     o.navigate = function (route) {
         window.history.pushState(null, null, route)
-        root.innerHTML = templateStore[route]
+        root.innerHTML = decodeURI(templateStore[route])
         o.run.apply(o)
     }
     window.calo = o || {
--- static/js/utils.js
+++ static/js/utils.js
@@ -16,14 +16,14 @@ function redirect(path) {
     location.href = path
 }
 
-function getHtmlOrJson(success) {
+function getHtmlOrJson(url, success) {
     function makeHttpObject() {
         if ("XMLHttpRequest" in window) return new XMLHttpRequest();
         else if ("ActiveXObject" in window) return new ActiveXObject("Msxml2.XMLHTTP");
     }
 
     var request = makeHttpObject();
-    request.open("GET", "/", true);
+    request.open("GET", url, true);
     request.send(null);
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
