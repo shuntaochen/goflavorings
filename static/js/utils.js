5c338d538cbf98977e45d8b94e34166aba466c87
Chen <359000081@qq.com>
2022-06-02 09:24:45 +0800

    add a dragger object;

--- static/js/utils.js
+++ static/js/utils.js
@@ -114,5 +114,45 @@ function requireAll(scripts, next) {
 }
 
 
+const dragger = {
+    setSrc: function (el, clone) {
+        this.src = el
+        this.clone = clone
+        el.draggable = true
+        el.addEventListener('dragstart', function () {
+            setTimeout(() => {
+                console.log(this);
+            }, 10)
+        });
 
+        el.addEventListener('dragend', function () {
+        });
+        return this
+    }, setTargets: function () {
+        for (var i of arguments) {
+            i.addEventListener('dragenter', dragEnter);
+            i.addEventListener('dragover', dragOver);
+            i.addEventListener('dragleave', dragLeave);
+            i.addEventListener('drop', drop);
+        }
 
+        function dragEnter() {
+            this.className += " holding";
+        }
+
+        function dragOver(e) {
+            e.preventDefault();
+        }
+
+        function dragLeave() {
+            this.className = 'empty';
+        }
+
+        function drop() {
+            console.log('drop')
+            this.className = 'empty';
+            let el = dragger.clone ? dragger.src.cloneNode(true) : dragger.src
+            this.append(el)
+        }
+    },
+}
