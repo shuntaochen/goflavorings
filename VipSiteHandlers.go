231ddb99332d084fcacfab9b8c8c764fbe25c26e
Calos <359000081@qq.com>
2022-06-05 10:35:32 +0800

    Move mylog method to helper.go in main package;

--- VipSiteHandlers.go
+++ VipSiteHandlers.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"flavorings/domain"
 )
 
 type VipSiteHandlers struct {
 }
 
 func (h *VipSiteHandlers) Login(w http.ResponseWriter, r *http.Request) {
+	domain.User d:=New()
+	
 	json.NewEncoder(w).Encode(map[string]bool{"ok": false})
 }
