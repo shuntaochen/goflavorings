4358bb2e4df8db046c03645e3d2a48fa4e07688d
Calos <359000081@qq.com>
2022-06-05 10:42:11 +0800

    Updated model created on with unitxmilli()

--- VipSiteHandlers.go
+++ VipSiteHandlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flavorings/domain"
 	"net/http"
+	"time"
 )
 
 type VipSiteHandlers struct {
@@ -13,6 +14,7 @@ func (h *VipSiteHandlers) Login(w http.ResponseWriter, r *http.Request) {
 	var d *domain.User = new(domain.User)
 	mylog(d)
 	d.Code = "hello world"
+	d.CreatedOn = time.Now().UnixMilli()
 	// json.NewEncoder(w).Encode(map[string]bool{"ok": false})
 	json.NewEncoder(w).Encode(d)
 }
